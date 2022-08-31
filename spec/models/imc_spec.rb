39437b055c2040f1322852b328edea0a3eb1df8a
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-08-31 20:34:47 -0300

    creating tests of validations for the model imc

--- spec/models/imc_spec.rb
+++ spec/models/imc_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+# Test suite for the IMC model
 RSpec.describe Imc, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  # ensure columns height and weight are present before saving
+  it { should validate_presence_of(:height) }
+  it { should validate_presence_of(:weight) }
+
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
