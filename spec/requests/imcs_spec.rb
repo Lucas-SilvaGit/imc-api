f315773f00b5e3a2d97ef884e46a343026cbc1de
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-09-01 05:11:16 -0300

    created tests for when request is valid and when request is invalid

--- spec/requests/imcs_spec.rb
+++ spec/requests/imcs_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Imcs", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Imc API', type: :request do
+
+  describe 'POST /imc' do
+    let(:valid_attributes) { { height: 1.74, weight: 62 } }
+
+    context 'when the request is valid' do
+      before { post '/imc', params: valid_attributes }
+
+      it 'return json' do
+        expect(json['imc']).to eq(20.47)
+        expect(json['classification']).to match("Peso normal ou adequado")
+        expect(json['obesity']).to match("0") 
+      end
+      
+      it 'returns status ok' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/imc', params: { height: 1.72} }
+
+      it 'returns status bad_request' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'body is empty' do
+        expect(response.body).to match("must be greater than 0")
+      end
+    end
   end
 end
