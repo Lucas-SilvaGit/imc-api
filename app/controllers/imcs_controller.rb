c1393b8379dec5174783d38344b9f1bd5e0ad7ff
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-09-01 00:09:58 -0300

    create controller method post

--- app/controllers/imcs_controller.rb
+++ app/controllers/imcs_controller.rb
@@ -1,2 +1,20 @@
 class ImcsController < ApplicationController
+  before_action :set_imc, only: [:show, :update, :destroy]
+
+  # POST /imcs
+  def create
+    @imc = Imc.create!(imc_params)
+    json_response(@imc, :created)
+  end
+
+  private
+
+  def imc_params
+    # whitelist params
+    params.permit(:height, :weight)
+  end
+
+  def set_todo
+    @imc = Imc.find(params[:id])
+  end
 end
